b9bca45aa5c3de510bb99805ccc01e695fd87af6
Farah <Farah@DESKTOP-4IKORCG.lan>
2025-01-29 20:12:31 -0600

    java is fun

--- src/Demo1/DemoTest3.java
+++ src/Demo1/DemoTest3.java
@@ -3,6 +3,12 @@ package Demo1;
 public class DemoTest3 {
 	public static void main(String args []) {
 		System.out.println("Java");
+		
+		int a = 20;
+		int b = 30;
+				int result = b-a;
+				
+				System.out.println(result);
 	}
 
 }
